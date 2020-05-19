e25fc4aa34386febbf45f41299ec7f0834e0f5ce
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-19 12:49:58 -0700

    begin mergesort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,43 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+def merge(array_left, array_right):
 
-    # Your code here
+    total_entries = len(array_left) + len(array_right)
+    merged_array = [0] * total_entries
 
+    # initialize pointers for start of each array
+    next_smallest_left_index = 0
+    next_smallest_right_index = 0
+
+    next_smallest_value_in_left_array = array_left[next_smallest_left_index]
+    next_smallest_value_in_right_array = array_right[next_smallest_right_index]
+
+    for i in range(total_entries):
+
+        # add an entry from the left array if...
+        #    there is a value in the left array but no values remaining in the right array
+        #    both values exist and the left value is smaller
+        if (next_smallest_value_in_left_array and not next_smallest_value_in_right_array) or (next_smallest_value_in_left_array < next_smallest_value_in_right_array):
+            merged_array[i] = next_smallest_value_in_left_array
+            next_smallest_left_index += 1
+        
+        # add an entry from the right array if...
+        #    there is a value in the right array but no values remaining in the left array
+        #    both values exist and the right value is smaller
+        elif (next_smallest_value_in_right_array and not next_smallest_value_in_left_array) or (next_smallest_value_in_right_array <= next_smallest_value_in_left_array):
+            merged_array[i] = next_smallest_value_in_right_array
+            next_smallest_right_index += 1
 
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
-    # Your code here
+    
+    start_of_right_half = len(arr) // 2
+
+    left_half = arr[0:start_of_right_half - 1]
+    right_half = arr[start_of_right_half:]
+
 
 
     return arr
