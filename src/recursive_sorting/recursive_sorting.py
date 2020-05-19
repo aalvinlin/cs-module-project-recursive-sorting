6b17291cbd1c8d8c4022a2e4a6bd5a65d81eb7d4
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-19 13:47:02 -0700

    simplify logic in merge helper by using more cases

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -8,24 +8,29 @@ def merge(array_left, array_right):
     next_smallest_left_index = 0
     next_smallest_right_index = 0
 
-    next_smallest_value_in_left_array = array_left[next_smallest_left_index]
-    next_smallest_value_in_right_array = array_right[next_smallest_right_index]
+    print("    merging...", array_left, array_right, total_entries)
 
     for i in range(total_entries):
 
-        # add an entry from the left array if...
-        #    there is a value in the left array but no values remaining in the right array
-        #    both values exist and the left value is smaller
-        if (next_smallest_value_in_left_array and not next_smallest_value_in_right_array) or (next_smallest_value_in_left_array < next_smallest_value_in_right_array):
-            merged_array[i] = next_smallest_value_in_left_array
+        # if the array_left has no more elements to add, append the rest of array_right and return
+        if next_smallest_left_index >= len(array_left):
+            return merged_array.extend(array_right[next_smallest_right_index:])
+
+        # if the array_right has no more elements to add, append the rest of array_left and return
+        elif next_smallest_right_index >= len(array_right):
+            return merged_array.extend(array_left[next_smallest_left_index:])
+
+        # add an entry from the left array if the left value is smaller
+        elif array_left[next_smallest_left_index] < array_right[next_smallest_right_index]:
+            merged_array[i] = array_left[next_smallest_left_index]
             next_smallest_left_index += 1
         
-        # add an entry from the right array if...
-        #    there is a value in the right array but no values remaining in the left array
-        #    both values exist and the right value is smaller
-        elif (next_smallest_value_in_right_array and not next_smallest_value_in_left_array) or (next_smallest_value_in_right_array <= next_smallest_value_in_left_array):
-            merged_array[i] = next_smallest_value_in_right_array
+        # add an entry from the right array if the right value is smaller
+        else:
+            merged_array[i] = array_right[next_smallest_right_index]
             next_smallest_right_index += 1
+
+    print("    merged array is", merged_array)
 
     return merged_array
 
@@ -33,17 +38,21 @@ def merge(array_left, array_right):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
 
+    print("input is", arr, "with length", len(arr))
+
     # base case: an array of a single element is sorted
-    if len(arr) == 1:
+    if len(arr) <= 1:
         return arr
     
     # divide array into left and right halves
     start_of_right_half = len(arr) // 2
 
-    left_half = arr[0:start_of_right_half - 1]
+    left_half = arr[0:start_of_right_half]
     right_half = arr[start_of_right_half:]
 
-    return merge(merge_sort(left_half, right_half))
+    print("  left and right are", left_half, right_half)
+
+    return merge(merge_sort(left_half), merge_sort(right_half))
 
 
 # implement an in-place merge sort algorithm
